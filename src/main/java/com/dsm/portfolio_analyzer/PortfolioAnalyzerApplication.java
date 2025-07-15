58ef9423132dfeecb23f7edbf76ab51e1946b291
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-07-15 15:40:58 +0530

    Initial commit

---
+++ src/main/java/com/dsm/portfolio_analyzer/PortfolioAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.dsm.portfolio_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsm/portfolio_analyzer/PortfolioAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsm.portfolio_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
